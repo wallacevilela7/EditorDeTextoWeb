b23e0ccf4285af0e0df75b5d964869aec256b560
Wallace Vilela <wallace.vilela9@gmail.com>
2024-04-18 19:11:38 -0300

    Definindo DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace EditorTexto.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options){}
     }
 }
