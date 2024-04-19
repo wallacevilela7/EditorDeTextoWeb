024bc61df556af9c946c2465aafc123e82531f56
Wallace Vilela <wallace.vilela9@gmail.com>
2024-04-19 18:36:51 -0300

    Adicionando service do banco ao Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using EditorTexto.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection"));
+});
 
 var app = builder.Build();
 
