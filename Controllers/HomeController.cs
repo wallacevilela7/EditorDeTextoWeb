27c07f63316721d6dca472f160586a41b1b148a8
Wallace Vilela <wallace.vilela9@gmail.com>
2024-04-22 21:34:26 -0300

    Construtor do controller home + inj dep

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EditorTexto.Models;
+using EditorTexto.Data;
 
 namespace EditorTexto.Controllers;
 
 public class HomeController : Controller
 {
 
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
         return View();
