d577e2a0cec2554a2e26af852813c944fc5041cc
Wallace Vilela <wallace.vilela9@gmail.com>
2024-04-22 21:35:06 -0300

    Definindo DbSet de Documento

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EditorTexto.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EditorTexto.Data
@@ -10,5 +11,7 @@ namespace EditorTexto.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) 
         : base(options){}
+
+        public DbSet<Documento> Documentos { get; set; }
     }
 }
